35252e7d1865c4f3e64888ead3d700c9ab0c4b97
ymron0 <github.8rh0v@passmail.com>
2026-09-12 10:05:58 +0200

    Moved files for coherent architecture

--- lib/src/features/assets/di/get_valuation_asset_service_provider.dart
+++ lib/src/features/assets/di/get_valuation_asset_service_provider.dart
@@ -1,4 +1,4 @@
-import 'package:axiom/src/application/services/get_valuation_asset_service.dart';
+import 'package:axiom/src/features/assets/application/services/get_valuation_asset_service.dart';
 import 'package:axiom/src/features/assets/di/get_asset_by_id_use_case_provider.dart';
 import 'package:axiom/src/features/settings/di/get_settings_use_case_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
